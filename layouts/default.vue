eb68e20e861ff935aedca7813d1071a2fc460649
The Coding Montana <63234437+DevHumbleChris@users.noreply.github.com>
2023-07-25 12:30:15 +0000

    saving changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,15 +4,15 @@ useHead({
     lang: 'en',
   },
   meta: [
-    { property: 'og:title', content: `Hounter` },
+    { property: 'og:title', content: `Hounter - House Hunter` },
     { name: 'description', content: 'Hounter - House Hunter built with Nuxt 3 and TailwindCSS.' },
     { property: 'og:description', content: 'Hounter - House Hunter built with Nuxt 3 and TailwindCSS.' },
-    { property: 'og:url', content: 'https://jadoo-ke.vercel.app/' },
-    { name: 'twitter:title', content: 'Jadoo' },
+    { property: 'og:url', content: 'https://hounter-ke.vercel.app/' },
+    { name: 'twitter:title', content: 'Hounter - House Hunter' },
     { name: 'twitter:description', content: 'Hounter - House Hunter built with Nuxt 3 and TailwindCSS.' },
-    { name: 'twitter:site', content: 'https://jadoo-ke.vercel.app/' }
+    { name: 'twitter:site', content: 'https://hounter-ke.vercel.app/' }
   ],
-  title: `Jadoo`,
+  title: `Hounter - House Hunter`,
 })
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script setup>
+import Hero from '~/components/hero/index.vue'
+</script>
+
 <template>
-    <h1 class="text-green-400">Welcome To Hounter</h1>
+    <Hero />
 </template>
