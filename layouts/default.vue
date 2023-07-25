0cc187104e8462969686b4fa51b79a0daa7307a1
DevHumbleChris <dev.humblechris254@gmail.com>
2023-07-25 06:37:58 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  meta: [
+    { property: 'og:title', content: `Hounter` },
+    { name: 'description', content: 'Hounter - House Hunter built with Nuxt 3 and TailwindCSS.' },
+    { property: 'og:description', content: 'Hounter - House Hunter built with Nuxt 3 and TailwindCSS.' },
+    { property: 'og:url', content: 'https://jadoo-ke.vercel.app/' },
+    { name: 'twitter:title', content: 'Jadoo' },
+    { name: 'twitter:description', content: 'Hounter - House Hunter built with Nuxt 3 and TailwindCSS.' },
+    { name: 'twitter:site', content: 'https://jadoo-ke.vercel.app/' }
+  ],
+  title: `Jadoo`,
+})
+</script>
+
+<template>
+  <main class="max-w-8xl">
+    <slot/>
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Welcome To Hounter</h1>
+</template>
